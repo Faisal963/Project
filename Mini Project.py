a6409f16d19abee21622aca5349801ee6d595387
Faisal963 <faisal.fefo963@gma>
2018-10-15 21:20:51 +0200

    programma uurloon

--- Mini Project.py
+++ Mini Project.py
@@ -1,19 +1,22 @@
 import csv
 
-with open ('fietsFile.csv', 'w+',newline='') as myCSVFile:
+with open ('fietsFile.csv', 'r+',newline='') as myCSVFile:
     persoonelijkInfo = csv.writer(myCSVFile, delimiter=';')
-    persoonelijkInfo.writerow(('fietsnum','naam','ov-num'))
+    persoonelijkInfo.writerow({'fietsnum','naam','ov-num'})
 
     while True:
-        fietsNum = input('Voer jou fiets nummer in: ')
         naam = input('Voer jou naam in: ')
-        ovNum = input('Voer je OV-Nummer in: ')
+        if naam is '':
+            break
+        if len(naam) >= 21:
+            print('Jij hebt te veel letters ingevoerd, probeer het opnieuw !')
+            print(naam)
+        fietsNum = input('Voer jou fiets nummer in: ')
 
         if len(fietsNum) < 4 and len(fietsNum)> 4 :
             print('Jij hebt een verkeerde fiets nummer ingvoerd, probeer het opnieuw !')
             print(fietsNum)
-        if len(naam) >= 21:
-            print('Jij hebt te veel letters ingevoerd, probeer het opnieuw !')
-            print(naam)
+        ovNum = input('Voer je OV-Nummer in: ')
         if len(ovNum) > 16:
             print('Onjuist OV-nummer ingevoerd, probeer het opnieuw !')
+        persoonelijkInfo.writerow({naam,ovNum,fietsNum})
