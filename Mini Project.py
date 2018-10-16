b91421c3103365dd7639b819d8ba4ce9d4cc1825
Faisal963 <faisal.fefo963@gma>
2018-10-16 22:38:48 +0200

    Mini proj

--- Mini Project.py
+++ Mini Project.py
@@ -1,5 +1,7 @@
+import os
+import csv
+
 def startScreen():
-    import os
     print()
     print('Welcome to NS Fietsen Stalling')
     print('1. Ik wil mijn fiets stallen')
@@ -8,9 +10,10 @@ def startScreen():
     print('4. Ik wil informatie opvragen')
     print('5. Ik wil stoppen')
     print()
+
     choice = input('Voer je keus in: ')
     if choice == '1':
-        fietsStallen()
+        Stallen()
     elif choice == '2':
         registreren()
     elif choice == '3':
@@ -26,13 +29,28 @@ def startScreen():
         startScreen()
 
 
-def fietsStallen():
-    import csv
+def stoppen():
+    print('Tot ziens')
+    grapje = input('Houd je van grappen ?anwoord op j/n:').capitalize()
+    if grapje == 'J':
+        startScreen()
+    else:
+        print('Jij mag weg\nTotziens !!')
 
+
+
+
+def stallen():
     with open ('fietsFile.csv', 'r+') as myCSVFile:
         persoonelijkInfo = csv.reader(myCSVFile)
-        for stalling in persoonelijkInfo:
-            fietsNum =
+        ovnummer = input('Vul je OV-nummer in: ')
+        for ovNum in persoonelijkInfo:
+            if ovNum in persoonelijkInfo:
+                print('Ga maar je fiets stallen')
+            else:
+                print('Jij moet je eerst registreren')
+                startScreen()
+
 
 
 def registreren():
@@ -40,33 +58,53 @@ def registreren():
 
     with open ('fietsFile.csv', 'w',newline='') as myCSVFile:
         persoonelijkInfo = csv.writer(myCSVFile, delimiter=';')
-        persoonelijkInfo.writerow({'fietsnum','naam','ov-num','e-mail','ww'})
+        persoonelijkInfo.writerow({'fietsnum','naam','ov_num','e-mail','ww'})
 
         naam = input('Voer jou (voor en achter) naam in: ')
+
         if naam is '':
             print('Ongeldige invoer, type je naam opnieuw')
             print(naam)
+
         elif len(naam) >= 31:
             print('Jij hebt te veel letters ingevoerd, probeer het opnieuw !')
             print(naam)
+
         fietsNum = input('Voer jou fiets nummer in: ')
+
         if len(fietsNum) < 4 and len(fietsNum)> 4 :
             print('Jij hebt een verkeerde fiets nummer ingvoerd, probeer het opnieuw !')
             print(fietsNum)
+
         ovNum = input('Voer je OV-Nummer in: ')
+
         if len(ovNum) > 16:
             print('Onjuist OV-nummer ingevoerd, probeer het opnieuw !')
+
         e_mail = input('Schrijf je e-mail adres: ')
+
         for mail in e_mail:
             mail = '@'
             if mail not in e_mail:
                 print('Het is ongeldig e-mail, probeer het opnieuw')
                 print(e_mail)
+
         ww = input('Vul een wacht woord in(bestaat uit 4 tekens): ')
+
         if len(ww) < 4 and len(ww) > 4 :
             print('Onjuist invoer, probeer het opnieuw !')
             print(ww)
+
         persoonelijkInfo.writerow({ovNum,naam,fietsNum,e_mail,ww})
     #return to start
     startScreen()
+
+def infoLezen():
+    gegevens = {}
+    with open('fietsFile.csv', 'r+') as myCSVFile:
+        for info in myCSVFile.readlines():
+            ov_num, naam = info.split(",")
+            gegevens[ov_num] = naam.strip()
+
+    return gegevens
 
